ab65adde2dcdecd5dcd58e9fa141b0a32efe25b6
Akkuman <akkumans@qq.com>
2023-05-08 09:47:21 +0800

    fix: 无法联网将会导致无法激活

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 127.0.0.1 updates.acunetix.com >> /etc/hosts
-echo 127.0.0.1 erp.acunetix.com >> /etc/hosts
-echo 127.0.0.1 telemetry.invicti.com >> /etc/hosts
+# echo 127.0.0.1 updates.acunetix.com >> /etc/hosts
+# echo 127.0.0.1 erp.acunetix.com >> /etc/hosts
+# echo 127.0.0.1 telemetry.invicti.com >> /etc/hosts
 
 exec "$@"
